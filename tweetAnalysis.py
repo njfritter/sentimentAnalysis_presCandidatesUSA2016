f8698e34fb6784517442556ae1b2afbe94449c43
njfritter <nathan.fritter@yahoo.com>
2017-01-08 22:27:46 -0800

    Fixed researched labelled corpuses; only Amazon works so far with accuracy of 80%

--- tweetAnalysis.py
+++ tweetAnalysis.py
@@ -37,12 +37,8 @@ from sklearn import metrics
 from sklearn.grid_search import GridSearchCV
 
 #from pandas.DataFrame import query
-"""
-import pygal
-from IPython.display import SVG
-import operator as o
-"""
 
+# Here are the files we will be manipulating and creating
 file = "tweets.csv"
 randomized_file = "randomized_tweets.csv"
 training_data = "training_data.csv"
@@ -50,7 +46,21 @@ testing_data = "testing_data.csv"
 unlabeled_data = "unlabeled.csv"
 predicted_data_NB = "predicted_nb.csv"
 predicted_data_LSVM = "predicted_lsvm.csv"
+amazon_reviews = "sentimentLabelledSentences/amazonLabelled.csv"
+imdb_reviews = "sentimentLabelledSentences/imdbLabelled.csv"
+yelp_reviews = "sentimentLabelledSentences/yelpLabelled.csv"
+randomized_amazon_file = "randomized_amazon_reviews.csv"
+randomized_imdb_file = "randomized_imdb_reviews.csv"
+randomized_yelp_file = "randomized_yelp_reviews.csv"
+amazon_training_data = "amazon_training_data.csv"
+amazon_testing_data = "amazon_testing_data.csv"
+imdb_training_data = "imdb_training_data.csv"
+imdb_testing_data = "imdb_testing_data.csv"
+yelp_training_data = "yelp_training_data.csv"
+yelp_testing_data = "yelp_testing_data.csv"
 
+
+# Define columns for my labelled data sets
 columns = [
     "row_id",
     "tweet_id",
@@ -90,6 +100,13 @@ unlabeled_columns = [
     "hour",
     "president",
     "tweet",
+    "label"
+    ]
+
+# Label columns in researcher labelled stuff
+research_columns = [
+    "row",
+    "review", 
     "label"
     ]
 
@@ -219,47 +236,23 @@ def tokenize_row_write(file_csv_writer, row_id, tweet_id, month, day, hour, pres
 
     file_csv_writer.writerow([row_id] + [tweet_id] + [month] + [day] + [hour] + [president]+ [words_tweet] + [label])
 
-"""
-Since gathering the frequencies is a repetitive task
-Can we create a function for it???
-"""
-"""   
-def gather_freq(president, label, raw_timestamp):
-    if president == "HillaryClinton":
-        if label == "positive":
-            pos_freq_clinton[raw_timestamp.tm_wday] += 1
-        if label == "negative":
-            neg_freq_clinton[raw_timestamp.tm_wday] += 1
-        else:
-            neu_freq_clinton[raw_timestamp.tm_wday] += 1
-
-    if president == "realDonaldTrump":
-        if label == "positive":
-            pos_freq_trump[raw_timestamp.tm_wday] += 1
-        if label == "negative":
-            neg_freq_trump[raw_timestamp.tm_wday] += 1
-        else:
-            neu_freq_trump[raw_timestamp.tm_wday] += 1
-    
-    else:
-        # Get frequencies of sentiment per day of week
-        # Index values of array used for day of week
-        if label == "positive":
-            pos_freq[raw_timestamp.tm_wday] += 1
-        if label == "negative":
-            neg_freq[raw_timestamp.tm_wday] += 1
-        else:
-            neu_freq[raw_timestamp.tm_wday] += 1
-"""    
-
 def data_viz(pos_freq, neu_freq, neg_freq, president):
-    
-    print("Positive frequencies by day of the week are: ")
-    print(pos_freq)
-    print("Neutral frequencies by day of the week are: ")
-    print(neu_freq)
-    print("Negative frequencies by day of the week are: ")
-    print(neg_freq)
+
+    if president == "":
+        print("Positive frequencies by day of the week are: ")
+        print(pos_freq)
+        print("Neutral frequencies by day of the week are: ")
+        print(neu_freq)
+        print("Negative frequencies by day of the week are: ")
+        print(neg_freq)
+
+    else:
+        print("Positive frequencies by day of the week for candidate {} are: ").format(president)
+        print(pos_freq)
+        print("Neutral frequencies by day of the week for candidate {} are: ").format(president)
+        print(neu_freq)
+        print("Negative frequencies by day of the candidate {} week are: ").format(president)
+        print(neg_freq)
         
     #Plot a multibar graph 
     numbers = np.arange(0, 7)
@@ -290,7 +283,7 @@ def data_viz(pos_freq, neu_freq, neg_freq, president):
     plt.show() 
  
 def extract_and_train():
-    
+    # Train based on my labeled dataset (which does contain my bias)
     train = pd.read_csv(training_data, names = train_columns)
     test = pd.read_csv(testing_data, names = test_columns)
     
@@ -330,6 +323,153 @@ def extract_and_train():
     # Call a Naive Bayes and Linear SVM algorithm on the data
     naive_bayes(train_words, y_train, test_words, y_test)
     linear_svm(train_words, y_train, test_words, y_test)
+
+def research_split():
+    # Train based on the researcher labeled corpus
+    # Which will be more objective than mine
+    # Files to be read in
+    """
+    amazon_file = pd.read_csv(amazon_reviews, names = research_columns)
+    imdb_file = pd.read_csv(imdb_reviews, names = research_columns)
+    yelp_file = pd.read_csv(yelp_reviews, names = research_columns)
+    """
+    # Some of the reviews only contained numbers with a "NA" label
+    # Adding in code to skip over those lines
+    # Also had to do some MAJOR data reformatting
+    # Link used here: 
+    # http://stackoverflow.com/questions/32127604/how-to-wrap-each-line-in-quotes-in-sublimetext
+    amazon_file = csv.reader(open(amazon_reviews, "rb+"))
+    imdb_file = csv.reader(open(imdb_reviews, "rb+"))
+    yelp_file = csv.reader(open(yelp_reviews, "rb+"))
+
+    # Files to be written
+    amazon_train_file = csv.writer(open(amazon_training_data, "wb+"))
+    amazon_test_file = csv.writer(open(amazon_testing_data, "wb+"))
+    imdb_train_file = csv.writer(open(imdb_training_data, "wb+"))
+    imdb_test_file = csv.writer(open(imdb_testing_data, "wb+"))
+    yelp_train_file = csv.writer(open(yelp_training_data, "wb+"))
+    yelp_test_file = csv.writer(open(yelp_testing_data, "wb+"))
+
+    """
+    # Randomize data
+    with open(amazon_file, 'rb') as source:
+        data = [ (random.random(), line) for line in source ]
+    data.sort()
+    with open(randomized_amazon_file, 'wb+') as target:
+        for _, line in data:
+            target.write( line )
+
+    with open(imdb_file, 'rb') as source:
+        data = [ (random.random(), line) for line in source ]
+    data.sort()
+    with open(randomized_imdb_file, 'wb+') as target:
+        for _, line in data:
+            target.write( line )
+
+    with open(yelp_file, 'rb') as source:
+        data = [ (random.random(), line) for line in source ]
+    data.sort()
+    with open(randomized_yelp_file, 'wb+') as target:
+        for _, line in data:
+            target.write( line )
+    
+    print("Files randomized")
+    prepped_amazon_file = csv.reader(open(randomized_amazon_file, "rb"))
+    prepped_imdb_file = csv.reader(open(randomized_imdb_file, "rb"))
+    prepped__file = csv.reader(open(randomized_imdb_file, "rb"))
+    
+    """
+
+    # replace files with "prepped" versions when possible
+    index = 0
+    for row in amazon_file:
+        (number, review, label) = row
+        ratio = 3
+
+        if index % ratio == 0:
+            tokenize_row_write_research(amazon_test_file, number, review, label)
+        else:
+            tokenize_row_write_research(amazon_train_file, number, review, label)
+            
+        index += 1
+
+    index = 0
+    for row in imdb_file:
+        (number, review, label) = row
+        ratio = 3
+
+        if index % ratio == 0:
+            tokenize_row_write_research(imdb_test_file, number, review, label)
+        else:
+            tokenize_row_write_research(imdb_train_file, number, review, label)
+            
+        index += 1
+
+    index = 0
+    for row in yelp_file:
+        (number, review, label) = row
+        ratio = 3
+
+        if index % ratio == 0:
+            tokenize_row_write_research(yelp_test_file, number, review, label)
+        else:
+            tokenize_row_write_research(yelp_train_file, number, review, label)
+            
+        index += 1
+
+def research_train():
+
+    amazon_train = pd.read_csv(amazon_training_data, names = research_columns)
+    amazon_test = pd.read_csv(amazon_testing_data, names = research_columns)
+    imdb_train = pd.read_csv(imdb_training_data, names = research_columns)
+    imdb_test = pd.read_csv(imdb_testing_data, names = research_columns)
+    yelp_train = pd.read_csv(yelp_training_data, names = research_columns)
+    yelp_test = pd.read_csv(yelp_testing_data, names = research_columns)
+
+    amazon_x_train = np.array((amazon_train['row'], ['review']))
+    amazon_y_train = np.array(amazon_train['label'])
+
+    amazon_x_test = np.array((amazon_test['row'], ['review']))
+    amazon_y_test = np.array(amazon_test['label'])
+
+    imdb_x_train = np.array((imdb_train['row'], ['review']))
+    imdb_y_train = np.array(imdb_train['label'])
+
+    imdb_x_test = np.array((imdb_test['row'], ['review']))
+    imdb_y_test = np.array(imdb_test['label'])
+
+    yelp_x_train = np.array((yelp_train['row'], ['review']))
+    yelp_y_train = np.array(yelp_train['label'])
+
+    yelp_x_test = np.array((yelp_test['row'], ['review']))
+    yelp_y_test = np.array(yelp_test['label'])
+
+    amazon_train_words = np.array(amazon_train['review'])
+    amazon_test_words = np.array(amazon_test['review'])
+
+    imdb_train_words = np.array(imdb_train['review'])
+    imdb_test_words = np.array(imdb_test['review'])
+
+    yelp_train_words = np.array(yelp_train['review'])
+    yelp_test_words = np.array(yelp_test['review'])
+
+    # Run functions on all three corpuses
+    print("\nThe results using Amazon reviews are:")
+    naive_bayes(amazon_train_words, amazon_y_train, amazon_test_words, amazon_y_test)
+    linear_svm(amazon_train_words, amazon_y_train, amazon_test_words, amazon_y_test)
+    
+    print("\nThe results using IMDB reviews are:")
+    naive_bayes(imdb_train_words, imdb_y_train, imdb_test_words, imdb_y_test)
+    linear_svm(imdb_train_words, imdb_y_train, imdb_test_words, imdb_y_test)
+    
+    print("\nThe results using Yelp reviews are:")
+    naive_bayes(yelp_train_words, yelp_y_train, yelp_test_words, yelp_y_test)
+    linear_svm(yelp_train_words, yelp_y_train, yelp_test_words, yelp_y_test)
+
+def tokenize_row_write_research(file_csv_writer, number, review, label):
+    
+    words_review = tokenized_string(review)
+    file_csv_writer.writerow([number] + [words_review] + [label])
 
 def naive_bayes(x_train, y_train, x_test, y_test):
     # Building a Pipeline; this does all of the work in extract_and_train() at once  
@@ -445,6 +585,12 @@ if __name__ == '__main__':
             extract_and_train()
         elif sys.argv[1] == 'compare':
             compare_predictions()
+    if len(sys.argv) == 3:
+        if sys.argv[1] == 'research':
+            if sys.argv[2] == 'split':
+                research_split()
+            if sys.argv[2] == 'train':
+                research_train()
 
 
 
